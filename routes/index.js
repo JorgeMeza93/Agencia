1befbbfd11fdb00788d7e7b3e74f0680a500589f
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-05-30 23:39:29 -0500

    Vista avanzado

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,29 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.render("layout/index");
-});
-router.get("/nosotros", (req, res)=> {
-    const viajes = "Viajes a Brasil";
-    res.render("nosotros", {
-        textoViajes: viajes,
+    res.render("layout/index", {
+        pagina: "Inicio"
     });
-
 });
-router.get("/contacto", (req, res)=> {
-    res.send("Contacto");
+router.get("/nosotros", (req, res)=>{
+    res.render("layout/nosotros", {
+        pagina: "Nosotros"
+    });
+});
+router.get("/contacto", (req, res)=>{
+    res.render("layout/contacto", {
+        pagina: "Contacto"
+    });
+});
+router.get("/viajes", (req, res)=> {
+    res.render("layout/viajes", {
+        pagina: "Viajes"
+    });
+});
+router.get("/testimoniales", (req, res)=>{
+    res.render("layout/testimoniales", {
+        pagina: "Testimoniales"
+    });
 });
 
 export default router;
